ec9e34e828ca6e16eb2826b363360d96f7a4d6b4
lokeshsonawane95 <lokeshsonawane95@gmail.com>
2022-05-22 20:03:20 +0530

    Added to MaxAmongThreeFloats: find max among three floats

--- GenericsTestMaximum/MaxAmongThree.cs
+++ GenericsTestMaximum/MaxAmongThree.cs
@@ -30,5 +30,23 @@ namespace GenericsTestMaximum
             }
             return num1;
         }
+
+        //Find maximum among three floats
+        public float MaxAmongThreeFloat(float num1, float num2, float num3)
+        {
+            if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0)
+            {
+                return num1;
+            }
+            if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0)
+            {
+                return num2;
+            }
+            if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0)
+            {
+                return num3;
+            }
+            return num1;
+        }
     }
 }
--- GenericsTestMaximum/Program.cs
+++ GenericsTestMaximum/Program.cs
@@ -9,6 +9,7 @@ namespace GenericsTestMaximum
             do
             {
                 Console.WriteLine("\n1. Find maximum among three integers");
+                Console.WriteLine("2. Find maximum among three floats");
 
                 Console.WriteLine("0. Exit");
                 Console.Write("Enter your choice : ");
@@ -26,6 +27,18 @@ namespace GenericsTestMaximum
 
                         //Test case 3 : Max Number in Third position
                         Console.WriteLine("Maximum integer is : " + maxAmongThree.MaxAmongThreeInt(1, 2, 3));
+                        break;
+                    case 2:
+                        MaxAmongThree maxAmongThree1 = new MaxAmongThree();
+
+                        //Test case 1 : Max Number in First position
+                        Console.WriteLine("Maximum float number is : " + maxAmongThree1.MaxAmongThreeFloat(3.1f, 2.1f, 1.1f));
+
+                        //Test case 2 : Max Number in Second position
+                        Console.WriteLine("Maximum float number is : " + maxAmongThree1.MaxAmongThreeFloat(1.1f, 3.1f, 2.1f));
+
+                        //Test case 3 : Max Number in Third position
+                        Console.WriteLine("Maximum float number is : " + maxAmongThree1.MaxAmongThreeFloat(1.1f, 2.1f, 3.1f));
                         break;
                     default:
                         Console.WriteLine("Please choose correct option from the list");
