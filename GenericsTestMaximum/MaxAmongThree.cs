af3e78d2d12b5a053b912baa702d503a25e44605
lokeshsonawane95 <lokeshsonawane95@gmail.com>
2022-05-22 20:00:49 +0530

    Added to MaxAmongThreeInts: find max among three integers

---
+++ GenericsTestMaximum/MaxAmongThree.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsTestMaximum
+{
+    //We are using CompareTo method to compare two variables
+    //Usage variable1.CompareTo(variable2)
+    //It returns 1: if variable1 is greater than variable2
+    //returns 0: if variable1 is equal to variable2 AND
+    //returns -1: if variable1 is less than variable2
+    internal class MaxAmongThree
+    {
+        //Find maximum among three integers
+        public int MaxAmongThreeInt(int num1, int num2, int num3)
+        {
+            if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0)
+            {
+                return num1;
+            }
+            if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0)
+            {
+                return num2;
+            }
+            if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0)
+            {
+                return num3;
+            }
+            return num1;
+        }
+    }
+}
--- GenericsTestMaximum/Program.cs
+++ GenericsTestMaximum/Program.cs
@@ -1,1 +1,37 @@
-﻿
+﻿using System;
+namespace GenericsTestMaximum
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int choice;
+            do
+            {
+                Console.WriteLine("\n1. Find maximum among three integers");
+
+                Console.WriteLine("0. Exit");
+                Console.Write("Enter your choice : ");
+                choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        MaxAmongThree maxAmongThree = new MaxAmongThree();
+
+                        //Test case 1 : Max Number in First position
+                        Console.WriteLine("Maximum integer is : " + maxAmongThree.MaxAmongThreeInt(3, 2, 1));
+
+                        //Test case 2 : Max Number in Second position
+                        Console.WriteLine("Maximum integer is : " + maxAmongThree.MaxAmongThreeInt(1, 3, 2));
+
+                        //Test case 3 : Max Number in Third position
+                        Console.WriteLine("Maximum integer is : " + maxAmongThree.MaxAmongThreeInt(1, 2, 3));
+                        break;
+                    default:
+                        Console.WriteLine("Please choose correct option from the list");
+                        break;
+                }
+            } while (choice != 0);
+        }
+    }
+}
